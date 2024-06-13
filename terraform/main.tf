f7f3c56e4b4e6dd037d27d07d5ba0c9d6a411669
yumotok <66120060+yumotok@users.noreply.github.com>
2024-06-13 18:38:03 +0900

    github actionsとAWSをOIDCで連携させるためのIAM系設定を追加

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,40 @@ terraform {
 
   required_version = ">= 1.2.0"
 }
+
+provider "aws" {
+  profile = "default"
+  region = "ap-northeast-1"
+}
+
+data "tls_certificate" "github_actions" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github_actions.certificates[0].sha1_fingerprint]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github_actions"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_actions.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+            "${aws_iam_openid_connect_provider.github_actions.url}:sub" = "repo:github.com/yumotok/home::ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
